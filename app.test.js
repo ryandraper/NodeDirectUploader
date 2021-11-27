7f436458626f943774c25d52f7986995e69dcb3d
ryandraper <ryandraper@users.noreply.github.com>
2021-11-26 21:07:08 -0800

    Adding basic test

---
+++ app.test.js
@@ -0,0 +1,12 @@
+const app = require('.app.js');
+
+describe('app test', () => {
+
+    test('basic test for demo', () => {
+        expect(1).toEqual(1);
+        expect(2).toEqual(2);
+        expect(3).toEqual(3);
+    });
+
+});
+
