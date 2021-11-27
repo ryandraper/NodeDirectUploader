2ef1c8382dae4afc3ab3a854373b0351c753e76a
ryandraper <ryandraper@users.noreply.github.com>
2021-11-26 21:53:42 -0800

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ describe('app test', () => {
     test('basic test for demo', () => {
         expect(1).toEqual(1);
         expect(2).toEqual(2);
-        expect(3).toEqual(4);
+        expect(3).toEqual(3);
     });
 
 });
