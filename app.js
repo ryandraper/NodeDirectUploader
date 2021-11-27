5c0ec47b587d43a291caba8460a1bc967f5ec77c
ryandraper <ryandraper@users.noreply.github.com>
2021-11-26 21:36:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,97 +11,100 @@
  * Import required packages.
  * Packages should be installed with "npm install".
  */
-const express = require('express');
-const aws = require('aws-sdk');
+function app(){
+  const express = require('express');
+  const aws = require('aws-sdk');
 
 
-/*
- * Set-up and run the Express app.
- */
-const app = express();
-app.set('views', './views');
-app.use(express.static('./public'));
-app.engine('html', require('ejs').renderFile);
-app.get('/', (req, res) => res.render('index.ejs'))
-app.listen(process.env.PORT || 3000);
+  /*
+  * Set-up and run the Express app.
+  */
+  const app = express();
+  app.set('views', './views');
+  app.use(express.static('./public'));
+  app.engine('html', require('ejs').renderFile);
+  app.get('/', (req, res) => res.render('index.ejs'))
+  app.listen(process.env.PORT || 3000);
 
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
+  // Parse URL-encoded bodies (as sent by HTML forms)
+  app.use(express.urlencoded());
 
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+  // Parse JSON bodies (as sent by API clients)
+  app.use(express.json());
 
-/*
- * Configure the AWS region of the target bucket.
- * Remember to change this to the relevant region.
- */
-aws.config.region = 'ca-central-1';
+  /*
+  * Configure the AWS region of the target bucket.
+  * Remember to change this to the relevant region.
+  */
+  aws.config.region = 'ca-central-1';
 
-/*
- * Load the S3 information from the environment variables.
- */
-const S3_BUCKET = process.env.S3_BUCKET;
+  /*
+  * Load the S3 information from the environment variables.
+  */
+  const S3_BUCKET = process.env.S3_BUCKET;
 
-/*
- * Respond to GET requests to /account.
- * Upon request, render the 'account.html' web page in views/ directory.
- */
-app.get('/account', (req, res) => res.render('account.html'));
+  /*
+  * Respond to GET requests to /account.
+  * Upon request, render the 'account.html' web page in views/ directory.
+  */
+  app.get('/account', (req, res) => res.render('account.html'));
 
-/*
- * Respond to GET requests to /sign-s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and
- * the anticipated URL of the image.
- */
-app.get('/sign-s3', (req, res) => {
-  const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
-  const fileType = req.query['file-type'];
-  const s3Params = {
-    Bucket: S3_BUCKET,
-    Key: fileName,
-    Expires: 60,
-    ContentType: fileType,
-    ACL: 'public-read'
-  };
-
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
-      console.log(err);
-      return res.end();
-    }
-    const returnData = {
-      signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+  /*
+  * Respond to GET requests to /sign-s3.
+  * Upon request, return JSON containing the temporarily-signed S3 request and
+  * the anticipated URL of the image.
+  */
+  app.get('/sign-s3', (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'public-read'
     };
-    res.write(JSON.stringify(returnData));
-    res.end();
-  });
-});
 
-/*
- * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in
- * a way that suits your application.
- */
-app.post('/save-details', (req, res) => {
-
-  let insertQuery = 'INSERT INTO users( username, fullname) VALUES(' + '\'' + req.body.username + '\', \'' + req.body.fullname + '\');';
-  console.log(insertQuery);
-
-  const { Client } = require('pg');
-  const client = new Client({
-      connectionString: process.env.DATABASE_URL,  
-      ssl: {
-        rejectUnauthorized: false
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+      if(err){
+        console.log(err);
+        return res.end();
       }
-  });
-  client.connect();
-  client.query(insertQuery, (err, result) => {
-    console.log(err ? err.stack : result); // Hello World!
-    client.end();
-    res.end('done');
+      const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      };
+      res.write(JSON.stringify(returnData));
+      res.end();
+    });
   });
 
-});
+  /*
+  * Respond to POST requests to /submit_form.
+  * This function needs to be completed to handle the information in
+  * a way that suits your application.
+  */
+  app.post('/save-details', (req, res) => {
 
+    let insertQuery = 'INSERT INTO users( username, fullname) VALUES(' + '\'' + req.body.username + '\', \'' + req.body.fullname + '\');';
+    console.log(insertQuery);
+
+    const { Client } = require('pg');
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,  
+        ssl: {
+          rejectUnauthorized: false
+        }
+    });
+    client.connect();
+    client.query(insertQuery, (err, result) => {
+      console.log(err ? err.stack : result); // Hello World!
+      client.end();
+      res.end('done');
+    });
+
+  });
+}
+
+module.exports.app = app;
